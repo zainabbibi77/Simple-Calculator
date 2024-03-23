243d56460ee89317d298fac4e6caffaf7c9f60a1
zainabibi <ismailzainab719@gmail.com>
2024-03-23 13:24:19 +0500

    Adding a Simple Calculator project

---
+++ app.ts
@@ -0,0 +1,35 @@
+#! /usr/bin/env node
+
+import inquirer from "inquirer";
+let answers = await inquirer.prompt([
+
+    {message: "Enter First number", type:"number" , name:"firstNumber"},
+    {message: "Enter Second number", type:"number", name:"secondNumber"},
+    {
+        message: "Select one Operator to perform Operations",
+        type: "list",
+        name: "operator",
+        choices: ["Addition", "Subtraction", "Multiplication", "Division",],
+
+    }
+]);
+console.log(answers);
+
+// conditional statements If - else
+if(answers.operator === "Addition"){
+    console.log(answers.firstNumber + answers.secondNumber);
+}
+else if(answers.operator === "Subtraction"){
+    console.log(answers.firstNumber - answers.secondNumber);
+}
+else if(answers.operator === "Multiplication"){
+    console.log(answers.firstNumber * answers.secondNumber);
+}
+else if(answers.operator === "Division"){
+    console.log(answers.firstNumber / answers.secondNumber);
+}
+else{
+    console.log("invalid input");
+}
+    
+
